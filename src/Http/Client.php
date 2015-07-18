fc6cff1a1ac2cd4e306e7b6623f96fa56d5326e5
akDeveloper <andreas.kollaros@gmail.com>
2015-07-18 22:50:44 +0300

    add license file. fix phpunit config

--- src/Http/Client.php
+++ src/Http/Client.php
@@ -2,10 +2,18 @@
 
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
+/*
+ * This file is part of the Larium Http Client package.
+ *
+ * (c) Andreas Kollaros <andreas@larium.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Larium\Http;
 
 use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
 use Zend\Diactoros\Response;
 use Larium\Http\Exception\CurlException;
 
--- src/Http/Exception/CurlException.php
+++ src/Http/Exception/CurlException.php
@@ -2,6 +2,15 @@
 
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
+/*
+ * This file is part of the Larium Http Client package.
+ *
+ * (c) Andreas Kollaros <andreas@larium.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Larium\Http\Exception;
 
 class CurlException extends ClientException
